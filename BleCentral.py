d48872e44e56e064ca84e005130d47e681072930
Jacob Lucero <jcblucero@gmail.com>
2020-09-07 22:46:58 -0400

    added characterisic UUIDs and added variables for chars

--- BleCentral.py
+++ BleCentral.py
@@ -6,10 +6,18 @@ See https://elinux.org/RPi_Bluetooth_LE for info on raspberrypi
 """
 
 from bluepy import btle
+import struct
 import time
 
 PIDTUNER_UUID = u'c83bb2d6757e47ae8947003fefbeadde'
 PID_UUID_STR = 'deadbeef-3f00-4789-ae47-7e75d6b23bc8'#PIDTUNER_UUID.decode('utf-8')
+THROTTLE_UUID = btle.UUID('deadbee1-3f00-4789-ae47-7e75d6b23bc8')
+KP_UUID = btle.UUID('deadbee2-3f00-4789-ae47-7e75d6b23bc8')
+KI_UUID = btle.UUID('deadbee3-3f00-4789-ae47-7e75d6b23bc8')
+KD_UUID = btle.UUID('deadbee4-3f00-4789-ae47-7e75d6b23bc8')
+START_STOP_UUID = btle.UUID('deadbeef-3f00-4789-ae47-7e75d6b23bc8')
+CCD_UUID = btle.UUID('00002902-0000-1000-8000-00805f9b34fb')
+
 print(PID_UUID_STR)
 PID_UUID = btle.UUID(PID_UUID_STR)
 print(PID_UUID)
@@ -72,6 +80,35 @@ if __name__ == "__main__":
             print("Service Found: {}".format(service.uuid),service.uuid.binVal)
             print(service.uuid.getCommonName())
         #time.sleep(5)
+
+        try:
+            pid_service = periph.getServiceByUUID(PID_UUID)
+            characteristics = pid_service.getCharacteristics()
+            for char in characteristics:
+                print(char)
+
+            throttle_characteristic = pid_service.getCharacteristics(THROTTLE_UUID)[0]
+            kp_characteristic = pid_service.getCharacteristics(KP_UUID)[0]
+            ki_characteristic = pid_service.getCharacteristics(KI_UUID)[0]
+            kd_characteristic = pid_service.getCharacteristics(KD_UUID)
+            start_stop_char = pid_service.getCharacteristics(START_STOP_UUID)
+
+            
+
+            print("THROTTLE ",throttle_characteristic.read())
+            print(struct.unpack('i',throttle_characteristic.read()))
+
+            print("kp ",throttle_characteristic.read())
+            print(struct.unpack('!i',throttle_characteristic.read()))
+
+            print("ki ",throttle_characteristic.read())
+            print(struct.unpack('i',throttle_characteristic.read()))
+            
+        except Exception as inst:
+            print(inst)
+            #print("Error finding {}".format(PID_UUID))
+
+        
 
         
 
